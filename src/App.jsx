0c0d6dd34696dec3cace6f7db202c57c896e2678
srivastavarahul442 <srivastavarahul442@gmail.com>
2026-05-29 17:18:09 +0530

    fix cors issue

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      {/* <h1 class="text-3xl font-bold underline">Hello world!</h1> */}
     </>
   );
 }
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from './NavBar'
+import Footer from './Footer'
 
 const Body = () => {
   return (
     <>
        <NavBar /> 
        <Outlet />
+       <Footer />
     </>
   )
 }
